980a5a6d396b5b1bdbd3705c3b758c6feffbea54
hkly <hannah.k.yiu@gmail.com>
2026-03-25 11:08:35 -0700

    Make placeholer ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import './style.scss'
 
+function generateNames() {
+  return Array(8).fill('Placeholder Name')
+}
+
 document.querySelector('#app').innerHTML = `
   <h1>Nakudama Name Generator</h1>
-  <p>Hello world!</p>
+  <button id="generate-btn">Generate Names</button>
+  <div id="results"></div>
 `
+
+document.querySelector('#generate-btn').addEventListener('click', () => {
+  const names = generateNames()
+  const results = document.querySelector('#results')
+  results.innerHTML = `
+    <ul class="name-grid">
+      ${names.map(name => `<li>${name}</li>`).join('')}
+    </ul>
+  `
+})
